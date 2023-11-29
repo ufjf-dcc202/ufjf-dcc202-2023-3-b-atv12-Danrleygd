0315f8849a6be25656297aeee103e988122a4a95
DanrleyGD <danrley.dias@estudante.ufjf.br>
2023-11-28 22:32:35 -0300

    cria origem e destino

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-document.entrada.addEventListener('submit', leFormulario);
+document.entrada.addEventListener("submit", leFormulario);
 
 function leFormulario(event) {
   event.preventDefault();
-  const quantidade = document.entrada.quantidade.value;
+  const quantidade = Number(document.entrada.quantidade.value);
   const fruta = document.entrada.fruta.value;
+  const origem = document.entrada.origem.value;
+  const destino = document.entrada.destino.value;
 
-  console.log(`Eu tenho ${quantidade} ${fruta}`);
+  console.log(`${origem} ${quantidade} ${fruta} para ${destino}`);
 }
