2413bc9e338932260f37af644c5256a734246029
DanrleyGD <danrley.dias@estudante.ufjf.br>
2023-12-05 22:54:15 -0300

    refatora codigo

--- estoque.js
+++ estoque.js
@@ -14,16 +14,63 @@ function getEstoque() {
 }
 
 function transacao(origem, destino, tipo, quantidade) {
+  if (origem === destino) {
+    return;
+  }
   if (destino === "pomar") {
+    dePessoaParaPomar(origem, tipo, quantidade);
+    return;
+  }
+  if (origem === "pomar") {
+    dePomarParaPessoa(destino, tipo, quantidade);
+    return;
+  }
+
+  const pessoaOrigem = estoque[origem];
+  const pessoaDestino = estoque[destino];
+  let monteOrigem;
+
+  for (let i = 0; i < pessoaOrigem.length; i++) {
+    const monte = pessoaOrigem[i];
+    if (monte.tipo === tipo) {
+      monteOrigem = monte;
+      break;
+    }
+  }
+
+  if (!monteOrigem) {
+    return;
+  }
+  let monteDestino;
+  for (let i = 0; i < pessoaDestino.length; i++) {
+    const monte = pessoaDestino[i];
+    if (monte.tipo === tipo) {
+      monteDestino = monte;
+      break;
+    }
+  }
+  if (!monteDestino) {
+      monteDestino = { tipo: tipo, qnd: 0 };
+      pessoaDestino.push(monteDestino);
+  }
+
+
+  const qtdReal =Math.min(quantidade, monteOrigem.qnd);
+  monteDestino.qnd += qtdReal;
+  monteDestino.qnd -= qtdReal;
+
+  function dePessoaParaPomar(origem, tipo, quantidade) {
     const pessoa = estoque[origem];
     for (let i = 0; i < pessoa.length; i++) {
       const monte = pessoa[i];
       if (monte.tipo === tipo) {
         monte.qnd -= Math.min(quantidade, monte.qnd);
+        return;
       }
     }
   }
-  if (origem === "pomar") {
+
+  function dePomarParaPessoa() {
     const pessoa = estoque[destino];
     for (let i = 0; i < pessoa.length; i++) {
       const monte = pessoa[i];
